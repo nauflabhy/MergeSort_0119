c231f0516f91f2ad2a6d11910acf079bc8d9c0cb
nauflabhy <n4uf4l.ku@gmail.com>
2025-04-17 17:27:20 +0800

    MergeSort3

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int arr[20], B[20]
+int arr[20], B[20];
 
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan Panjang element array :";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else{
+            cout << "\nMaksimal panjang array 20";
+        }
+    }
+}
