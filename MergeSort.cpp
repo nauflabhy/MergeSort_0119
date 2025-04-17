877e479bd9f8bd622fc90d8532ff5e720d36686b
nauflabhy <n4uf4l.ku@gmail.com>
2025-04-17 17:35:37 +0800

    MergeSort6

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -30,3 +30,22 @@ void input()
         cin >> arr[i];
     }
 }
+
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high)
+}
